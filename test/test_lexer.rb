3848f050b54ede2c85a23243650079047a6295b8
Jarod Luebbert <jarod.luebbert@gmail.com>
2010-03-29 12:51:02 -0500

    Made the lexer test compare against the output of the first version of it.

--- test/test_lexer.rb
+++ test/test_lexer.rb
@@ -1,7 +1,8 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'lexer'
+require 'test/unit'
 
-code = <<-EOS
+CODE = <<-EOS
 class Wrapper:
     background = "rgb(255, 255, 255)"
     padding = 1
@@ -11,4 +12,12 @@ id Navigation:
     float = "left"
 EOS
 
-p Flair::Lexer.new(code).tokenize
+EXPECTED = <<-EOS
+[[:CLASS, "class"], [:CONSTANT, "Wrapper"], [:INDENT, 4], [:IDENTIFIER, "background"], ["=", "="], [:STRING, "rgb(255, 255, 255)"], [:NEWLINE, "\n"], [:IDENTIFIER, "padding"], ["=", "="], [:NUMBER, 1], [:DEDENT, 0], [:NEWLINE, "\n"], [:NEWLINE, "\n"], [:ID, "id"], [:CONSTANT, "Navigation"], [:INDENT, 4], [:IDENTIFIER, "background"], ["=", "="], [:STRING, "rgb(40, 40, 40)"], [:NEWLINE, "\n"], [:IDENTIFIER, "float"], ["=", "="], [:STRING, "left"], [:DEDENT, 0]]
+EOS
+
+class LexerTest < Test::Unit::TestCase
+  def test_tokenize
+    assert_equal EXPECTED, Flair::Lexer.new(CODE).tokenize
+  end
+end
